d87ca11387e760d4d1c430cc1a43308fa3d66a1d
valeaispuro <136286504+valeaispuro@users.noreply.github.com>
2025-03-14 23:39:53 -0600

    Update User.java

--- src/main/java/com/example/oci_microservice/model/User.java
+++ src/main/java/com/example/oci_microservice/model/User.java
@@ -11,7 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String studentId; // MATRICULA (String)
+    private String studentId;
 
     private String firstName;
     private String lastName;
